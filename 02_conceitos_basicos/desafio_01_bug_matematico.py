d5c9ce46403e3f327452f259da7a22b075278078
AirleyAlves <airleyalves0@gmail.com>
2026-09-15 12:52:51 -0300

    desafio bug média escolar

--- 02_conceitos_basicos/desafio_01_bug_matematico.py
+++ 02_conceitos_basicos/desafio_01_bug_matematico.py
@@ -20,3 +20,7 @@ SUA MISSÃO:
 # print("A média do aluno é:", media)
 
 # TODO: Escreva aqui o código corrigido:
+nota1=float(input("qual foi a primeira nota ? "))
+nota2=float(input("qual foi a segunda nota ? "))
+media=(nota1 + nota2)/ 2
+print(f"a média do aluno é {media}" )
